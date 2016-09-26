a90c647a7eb053818a4e0f084e65b11e1dacec68
Stanislav <stanislav.freelancer2014@gmail.com>
2016-09-26 19:03:12 +0300

    change mark from integer to double

--- common/models/Rating.php
+++ common/models/Rating.php
@@ -27,7 +27,7 @@ class Rating extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['mark'], 'integer', 'min' => 1, 'max' => 10],
+            [['mark'], 'double', 'min' => 1, 'max' => 10],
             [['title'], 'required'],
             [['title'], 'string', 'max' => 255],
         ];

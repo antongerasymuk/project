4cdf397b54598268e5f76898def7ef45817d8eba
Stanislav <stanislav.freelancer2014@gmail.com>
2016-09-27 12:13:54 +0300

    Migrations

---
+++ console/migrations/m160927_091206_remove_company_id.php
@@ -0,0 +1,18 @@
+<?php
+
+use yii\db\Migration;
+
+class m160927_091206_remove_company_id extends Migration
+{
+    public function up()
+    {
+        $this->dropColumn('directors', 'company_id');
+    }
+
+    public function down()
+    {
+        echo "m160927_091206_remove_company_id cannot be reverted.\n";
+
+        return false;
+    }
+}

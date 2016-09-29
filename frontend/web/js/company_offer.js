0c4da6fb0f15e07300d9d03c28b2e2e0159be11f
Anton <bron_off1@ukr.net>
2016-09-29 16:46:13 +0300

    add bonuses to index

--- frontend/web/js/company_offer.js
+++ frontend/web/js/company_offer.js
@@ -1,7 +1,9 @@
 
 riot.tag2('company-offer', '<div class="item" riot-style="background-color: {opts.bg_color};"> <div class="img"><a href="#"><img riot-src="{opts.logo}" alt=""></a></div> <h3><a href="#">{opts.title} Offers</a></h3> <p><a if="{ prices.casino }" href="#">Casino {prices.casino}£</a> <a if="{ prices.sport }" href="#">Sport  {prices.sport}£</a> <a if="{ prices.bingo }" href="#">Bingo  {prices.bingo}£</a> <a if="{ prices.poker }"  href="#">Poker  {prices.poker}£</a></p> </div>', '', '', function(opts) {
-console.log(opts);
-// prices=JSON.parse(opts.prices);
-//console.log(prices.casino);
+    console.log(JSON.stringify(opts));
+    prices = [];
+    for (i = 0; i < opts.reviews.length; i++) {
+        prices[opts.reviews[i].bonuses[0].title ] = opts.reviews[i].bonuses[0].price;
+    }
 });
-//riot.mount('company-offer')
+

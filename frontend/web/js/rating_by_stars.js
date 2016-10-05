429364f23982c949f5c9e6cfbb8e3b9ad6685cab
Anton <bron_off1@ukr.net>
2016-10-05 11:46:32 +0300

    fix stars and compatible in bonus by category

--- frontend/web/js/rating_by_stars.js
+++ frontend/web/js/rating_by_stars.js
@@ -1,11 +1,12 @@
 
-riot.tag2('rating-by-stars', '<div class="rating" > <span class="star flaticon-star-{rating_array[i]}" each="{ name, i in rating_array }"></span> </div>', '', '', function(opts) {
-rating_array =  ['empty','empty','empty','empty','empty'];
+riot.tag2('rating-by-stars', '<div class="rating" > <span class="star flaticon-star-{name}" each="{ name, i in opts.rating_array }"></span> </div>', '', '', function(opts) {
+opts.rating_array =  ['empty','empty','empty','empty','empty'];
 for( var i=0; i<parseInt(opts.rating/2);i++) {
-rating_array[i] = 'full';
+opts.rating_array[i] = 'full';
 }
 if (opts.rating%2!=0) {
-rating_array[parseInt(opts.rating/2)] ='half';
+opts.rating_array[parseInt(opts.rating/2)] ='half';
 }
+console.log(opts.rating_array);
 });
 riot.mount('rating-by-stars', {})

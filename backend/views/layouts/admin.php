3444e0c29f013e917898962741718d71a10ebfc6
Stanislav <stanislav.freelancer2014@gmail.com>
2016-10-12 11:39:52 +0300

    admin

---
+++ backend/views/layouts/admin.php
@@ -0,0 +1,200 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use backend\assets\AdminAsset;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\widgets\Breadcrumbs;
+
+AdminAsset::register( $this );
+?>
+
+<?php $this->beginPage() ?>
+	<!DOCTYPE html>
+	<html lang="<?= Yii::$app->language ?>">
+	<head>
+		<!-- start: Meta -->
+		<meta charset="<?= Yii::$app->charset ?>">
+		<?= Html::csrfMetaTags() ?>
+		<title><?= Html::encode( $this->title ) ?></title>
+		<!-- end: Meta -->
+
+		<!-- start: Mobile Specific -->
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<!-- end: Mobile Specific -->
+
+		<!-- The HTML5 shim, for IE6-8 support of HTML5 elements -->
+		<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+		<link id="ie-style" href="css/ie.css" rel="stylesheet">
+		<![endif]-->
+
+		<!--[if IE 9]>
+		<link id="ie9style" href="css/ie9.css" rel="stylesheet">
+		<![endif]-->
+		<!-- start: Favicon -->
+		<link rel="shortcut icon" href="/favicon.ico">
+		<!-- end: Favicon -->
+		<?php $this->head() ?>
+	</head>
+	<body>
+	<?php $this->beginBody() ?>
+	<!-- start: Header -->
+	<div class="navbar">
+		<div class="navbar-inner">
+			<div class="container-fluid">
+				<!-- start: Header Menu -->
+				<div class="nav-no-collapse header-nav">
+					<a class="brand" href="<?= Url::to(['site/index']) ?>"><span>Bonus Dashboard</span></a>
+
+                    <ul class="nav pull-right">
+                        <li class="dropdown">
+                            <a class="btn dropdown-toggle" data-toggle="dropdown" href="#">
+                                <i class="halflings-icon white user"></i> <?= Yii::$app->user->getIdentity()->username ?>
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a href="<?= Url::to(['setting/edit', 'id' => Yii::$app->user->getId()]) ?>"><i class="halflings-icon user"></i> Edit</a></li>
+                                <li><a href="<?= Url::to(['site/logout']) ?>"><i class="halflings-icon off"></i> Logout</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+				</div>
+				<!-- end: Header Menu -->
+			</div>
+		</div>
+	</div>
+	<!-- start: Header -->
+
+	<div class="container-fluid-full">
+		<div class="row-fluid">
+
+			<!-- start: Main Menu -->
+			<div id="sidebar-left" class="span2">
+				<div class="nav-collapse sidebar-nav">
+					<ul class="nav nav-tabs nav-stacked main-menu">
+						<li>
+							<a href="<?= Url::to(['site/index']) ?>">
+								<i class="icon-bar-chart"></i>
+								<span class="hidden-tablet">Dashboard</span>
+							</a>
+						</li>
+						<li>
+							<a href="<?= Url::to(['company/index']) ?>">
+								<i class="icon-building"></i>
+								<span class="hidden-tablet">Companies</span>
+							</a>
+						</li>
+						<li>
+							<a href="<?= Url::to(['page/index']) ?>">
+								<i class="icon-file"></i>
+								<span class="hidden-tablet">Pages</span>
+							</a>
+						</li>
+						<li>
+							<a href="<?= Url::to(['category/index']) ?>">
+								<i class="icon-book"></i>
+								<span class="hidden-tablet">Categories</span>
+							</a>
+						</li>
+						<li>
+							<a href="<?= Url::to(['country/index']) ?>">
+								<i class="icon-flag"></i>
+								<span class="hidden-tablet">Countries</span>
+							</a>
+						</li>
+						<li>
+							<a href="<?= Url::to(['os/index']) ?>">
+								<i class="icon-desktop"></i>
+								<span class="hidden-tablet">Operating systems</span>
+							</a>
+						</li>
+						<li>
+							<a href="<?= Url::to(['review/index']) ?>">
+								<i class="icon-eye-open"></i>
+								<span class="hidden-tablet">Reviews</span>
+							</a>
+						</li>
+						<li>
+							<a href="<?= Url::to(['payment/index']) ?>">
+								<i class="icon-credit-card"></i>
+								<span class="hidden-tablet">Payment methods</span>
+							</a>
+						</li>
+						<li>
+							<a href="<?= Url::to(['plus/index']) ?>">
+								<i class="icon-thumbs-up"></i>
+								<span class="hidden-tablet">Review pluses</span>
+							</a>
+						</li>
+                        <li>
+                            <a href="<?= Url::to(['minus/index']) ?>">
+                                <i class="icon-thumbs-down"></i>
+                                <span class="hidden-tablet">Review minuses</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="<?= Url::to(['license/index']) ?>">
+                                <i class="icon-lock"></i>
+                                <span class="hidden-tablet">Licenses</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="<?= Url::to(['director/index']) ?>">
+                                <i class="icon-user"></i>
+                                <span class="hidden-tablet">Directors</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="<?= Url::to(['setting/index']) ?>">
+                                <i class="icon-cogs"></i>
+                                <span class="hidden-tablet">Settings</span>
+                            </a>
+                        </li>
+					</ul>
+				</div>
+			</div>
+			<!-- end: Main Menu -->
+
+			<noscript>
+				<div class="alert alert-block span10">
+					<h4 class="alert-heading">Warning!</h4>
+					<p>You need to have <a href="http://en.wikipedia.org/wiki/JavaScript" target="_blank">JavaScript</a>
+						enabled to use this site.</p>
+				</div>
+			</noscript>
+			<!-- start: Content -->
+			<div id="content" class="span10">
+                <!-- Breadcrumbs here -->
+				<div class="row-fluid"><?= $content ?></div>
+			</div><!--/.fluid-container-->
+
+			<!-- end: Content -->
+		</div><!--/#content.span10-->
+	</div><!--/fluid-row-->
+
+	<div class="modal hide fade" id="myModal">
+		<div class="modal-header">
+			<button type="button" class="close" data-dismiss="modal">×</button>
+			<h3>Settings</h3>
+		</div>
+		<div class="modal-body">
+			<p>Here settings can be configured...</p>
+		</div>
+		<div class="modal-footer">
+			<a href="#" class="btn" data-dismiss="modal">Close</a>
+			<a href="#" class="btn btn-primary">Save changes</a>
+		</div>
+	</div>
+	<div class="clearfix"></div>
+	<footer>
+		<p>
+			<span style="text-align:left;float:left">© 2016 <a href="#" alt="">Bonus</a></span>
+		</p>
+	</footer>
+	<?php $this->endBody() ?>
+	</body>
+	</html>
+<?php $this->endPage() ?>
---
+++ backend/web/js/stan.custom.js
@@ -0,0 +1,216 @@
+// Create bonus event
+$('#bonus-create').on('click', function (e) {
+    e.preventDefault();
+
+    Model.create({
+        formId: 'bonus-create-form',
+        formIndex: 2,
+        createActionUrl: '/backend/bonus/create',
+        select2Selector: '#review-bonusids',
+        modalSelector: '#bonus-create-modal'
+    });
+});
+
+$('#rating-create').on('click', function (e) {
+    e.preventDefault();
+
+    Model.create({
+        formId: 'rating-create-form',
+        formIndex: 3,
+        createActionUrl: '/backend/rating/create',
+        select2Selector: '#review-ratingids',
+        modalSelector: '#rating-create-modal'
+    });
+});
+
+$('#plus-create').on('click', function (e) {
+    e.preventDefault();
+
+    Model.create({
+        formId: 'plus-create-form',
+        formIndex: 3,
+        createActionUrl: '/backend/plus/create',
+        select2Selector: '#review-plusids',
+        modalSelector: '#plus-create-modal'
+    });
+});
+
+$('#director-create').on('click', function (e) {
+    e.preventDefault();
+
+    Model.create({
+        formId: 'director-create-form',
+        formIndex: 6,
+        createActionUrl: '/backend/director/create',
+        select2Selector: '#company-director_id',
+        modalSelector: '#director-create-modal'
+    });
+});
+$('#license-create').on('click', function (e) {
+    e.preventDefault();
+
+    Model.create({
+        formId: 'license-create-form',
+        formIndex: 7,
+        createActionUrl: '/backend/license/create',
+        select2Selector: '#company-licenseids',
+        modalSelector: '#license-create-modal'
+    });
+});
+
+$('#minus-create').on('click', function (e) {
+    e.preventDefault();
+
+    Model.create({
+        formId: 'minus-create-form',
+        formIndex: 4,
+        createActionUrl: '/backend/minus/create',
+        select2Selector: '#review-minusids',
+        modalSelector: '#minus-create-modal'
+    });
+});
+
+$('#deposit-create').on('click', function (e) {
+    e.preventDefault();
+
+    Model.create({
+        formId: 'deposit-create-form',
+        formIndex: 5,
+        createActionUrl: '/backend/deposit/create',
+        select2Selector: '#review-depositids',
+        modalSelector: '#deposit-create-modal'
+    });
+});
+
+// Create review event
+$('#review-create').on('click', function (e) {
+    e.preventDefault();
+
+    Model.create({
+        formId: 'review-create-form',
+        formIndex: 1,
+        createActionUrl: '/backend/review/create',
+        select2Selector: '#company-reviewids',
+        modalSelector: '#review-create-modal'
+    });
+});
+
+function beforeSendHandler() {
+    Loader.show();
+}
+
+var validationEnabled = false;
+/**
+ * Validates form.
+ * Note: after first successful validation of form this state of form will be cached (this is yii.activeForm.js-native feature).
+ * @param {Function} callback Function as first argument passes result of validation.
+ */
+function validateForm(callback, $form) {
+
+    if (!validationEnabled) {
+        $form.on('submit', function () {
+            var $form       = $(this),
+                yiiFormData = $form.yiiActiveForm('data');
+
+            callback(yiiFormData.validated);
+
+            return false;                   // we stop submitting of form (we submit only for performing of validation)
+        });
+
+        validationEnabled = true;           // don't use of .one(handler), because it can't prevent form submitting
+    }
+
+
+    $form.trigger('submit');    // this runs validation of form
+}
+
+var Model = {
+    create: function (options) {
+        options.fileSelector = options.fileSelector || '.filename';
+
+        var $form = $('#' + options.formId);
+
+        tinyMCE.triggerSave();
+
+        validateForm(function (successValidated) {
+            if (successValidated) {
+                // !!!!!don't saved without this line
+                var form = document.getElementById(options.formId);
+                var formData = new FormData(form);
+                // submit form - saving file form via ajax
+                $.ajax({
+                    url: options.createActionUrl,
+                    type: 'POST',
+                    beforeSend: beforeSendHandler,
+                    data: formData,
+                    success: function (response) {
+                        Loader.hide();
+
+                        if (response.success) {
+                            Select2.appendItem(options.select2Selector, response.item);
+                            form.reset();
+
+                            Swalt.success('Success', 'Created');
+                            // clear file input
+                            $form.find(options.fileSelector).text('No file selected');
+                            $(options.modalSelector).modal('hide');
+                        } else {
+                            Swalt.warning('Ops!', 'Not saved');
+                        }
+                    },
+                    error: function (response) {
+                        Swalt.warning('Oops!', 'Please, check review fields and try again');
+                    },
+                    cache: false,
+                    contentType: false,
+                    processData: false
+                });
+            }
+        }, $form);
+    }
+};
+
+var Loader = {
+    loaderSelector: '#preloader',
+    submitBtnSelector: '#review-create',
+    append: function () {
+        $(this.submitBtnSelector).parent().append('<div id="preloader" class="preloader" style="display: none;"></div>')
+    },
+    show: function () {
+        this.append();
+        $(this.loaderSelector).show();
+    },
+    hide: function () {
+        $(this.loaderSelector).hide();
+        $(this.loaderSelector).remove();
+    }
+};
+
+var Swalt = {
+    alert: function (title, text, type) {
+        type = type || "success";
+
+        swal(title, text, type);
+    },
+    warning: function (title, text) {
+        this.alert(title, text, "error");
+    },
+    success: function (title, text) {
+        this.alert(title, text, "success");
+    }
+};
+
+var Select2 = {
+  appendItem: function (blockId, options) {
+      console.log(options);
+      var element = '<option value="'+ options.id +'">' + options.value + '</option>';
+      $(blockId).append(element);
+  }
+};
+
+
+function reviewAddressCallback(Editor) {
+    Editor.on('NodeChange', function(e){
+        tinyMCE.triggerSave();
+    });
+}

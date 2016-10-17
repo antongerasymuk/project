c8ba11b51822960564a850dad04679d971b60d82
Stanislav <stanislav.freelancer2014@gmail.com>
2016-10-17 17:50:48 +0300

    Backend styles

--- backend/assets/AdminAsset.php
+++ backend/assets/AdminAsset.php
@@ -12,7 +12,7 @@ class AdminAsset extends AssetBundle
 	public $basePath = '@webroot';
 	public $baseUrl = '@web';
 	public $css = [
-        'css/backend.css',
+        'css/backend.min.css',
 		'http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&subset=latin,cyrillic-ext,latin-ext'
 	];
 	public $js = [

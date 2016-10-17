10396ee0680f6622db51a928bdd8bbf2755c6534
Stanislav <stanislav.freelancer2014@gmail.com>
2016-10-17 17:25:06 +0300

    Remove login access actions Frontend

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -2,20 +2,10 @@
 namespace frontend\controllers;
 
 use common\models\Categorie;
-use common\models\Company;
 use common\models\Review;
 use Yii;
-use yii\base\InvalidParamException;
-use yii\web\BadRequestHttpException;
 use yii\web\Controller;
-use yii\filters\VerbFilter;
-use yii\filters\AccessControl;
-use common\models\LoginForm;
 use common\models\Site;
-use frontend\models\PasswordResetRequestForm;
-use frontend\models\ResetPasswordForm;
-use frontend\models\SignupForm;
-//use frontend\models\Contact;
 use frontend\models\ContactForm;
 use yii\web\NotFoundHttpException;
 
@@ -38,33 +28,9 @@ class SiteController extends Controller
                 'only' => ['index'],
                 'dependency' => [
                     'class' => 'yii\caching\DbDependency',
-                    'sql' => 'SELECT COUNT(*) FROM categories',
+                    'sql' => 'SELECT (SELECT COUNT(*) FROM categories) + (SELECT COUNT(*) FROM bonuses)',
                 ],
-            ],
-
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout', 'signup'],
-                'rules' => [
-                    [
-                        'actions' => ['signup'],
-                        'allow' => true,
-                        'roles' => ['?'],
-                    ],
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-
+            ]
         ];
     }
 
@@ -76,11 +42,7 @@ class SiteController extends Controller
         return [
             'error' => [
                 'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
+            ]
         ];
     }
 
@@ -154,39 +116,6 @@ class SiteController extends Controller
     }
 
     /**
-     * Logs in a user.
-     *
-     * @return mixed
-     */
-    public function actionLogin()
-    {
-        if (!Yii::$app->user->isGuest) {
-            return $this->goHome();
-        }
-
-        $model = new LoginForm();
-        if ($model->load(Yii::$app->request->post()) && $model->login()) {
-            return $this->goBack();
-        } else {
-            return $this->render('login', [
-                'model' => $model,
-            ]);
-        }
-    }
-
-    /**
-     * Logs out the current user.
-     *
-     * @return mixed
-     */
-    public function actionLogout()
-    {
-        Yii::$app->user->logout();
-
-        return $this->goHome();
-    }
-
-    /**
      * Displays contact page.
      *
      * @return mixed
@@ -207,123 +136,11 @@ class SiteController extends Controller
             'model' => $model,
         ]);
     }
-    /*$this->layout = "main_contact";
-
-    $model = new Contact();
-    var_dump($model);exit;
-    if ($model->load(Yii::$app->request->post()) && $model->save()) {
-
-    Yii::$app->session->setFlash('contactFormSubmitted');
-
-    return $this->render('contact', [
-
-    'model' => $model,
-
-    ]);
-
-    } else {
-
-    return $this->render('contact', [
-
-    'model' => $model,
-
-    ]);
-
-    }}*/
-
-
-    /**
-     * Displays about page.
-     *
-     * @return mixed
-     */
-    public function actionAbout()
-    {
-        return $this->render('about');
-    }
 
 
     public function actionSitemap()
     {
         return $this->render('sitemap');
-    }
-
-    /**
-     * Signs user up.
-     *
-     * @return mixed
-     */
-
-    /**
-     * Signs user up.
-     *
-     * @return mixed
-     */
-    public function actionSignup()
-    {
-        $model = new SignupForm();
-        if ($model->load(Yii::$app->request->post())) {
-            if ($user = $model->signup()) {
-                if (Yii::$app->getUser()->login($user)) {
-                    return $this->goHome();
-                }
-
-            }
-        }
-
-        return $this->render('signup', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Requests password reset.
-     *
-     * @return mixed
-     */
-    public function actionRequestPasswordReset()
-    {
-        $model = new PasswordResetRequestForm();
-        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
-            if ($model->sendEmail()) {
-                Yii::$app->session->setFlash('success', 'Check your email for further instructions.');
-
-                return $this->goHome();
-            } else {
-                Yii::$app->session->setFlash('error', 'Sorry, we are unable to reset password for email provided.');
-            }
-        }
-
-        return $this->render('requestPasswordResetToken', [
-            'model' => $model,
-        ]);
-    }
-
-    /**
-     * Resets password.
-     *
-     * @param string $token
-     *
-     * @return mixed
-     * @throws BadRequestHttpException
-     */
-    public function actionResetPassword($token)
-    {
-        try {
-            $model = new ResetPasswordForm($token);
-        } catch (InvalidParamException $e) {
-            throw new BadRequestHttpException($e->getMessage());
-        }
-
-        if ($model->load(Yii::$app->request->post()) && $model->validate() && $model->resetPassword()) {
-            Yii::$app->session->setFlash('success', 'New password was saved.');
-
-            return $this->goHome();
-        }
-
-        return $this->render('resetPassword', [
-            'model' => $model,
-        ]);
     }
 
     public function actionCategory($id = null)
@@ -332,10 +149,10 @@ class SiteController extends Controller
 
         if ($title = Categorie::findOne($id)) {
 
-        $title = Categorie::findOne($id)->title;
-        $this->view->params['category_title'] = $title;
+            $title = Categorie::findOne($id)->title;
+            $this->view->params['category_title'] = $title;
         } else {
-            throw new NotFoundHttpException('Unknown id of categorie');
+            throw new NotFoundHttpException('Unknown category');
         }
         return $this->render('bonuses_by_filter', ['title' => $title]);
     }

278d6037d9003f4035e7d4c87208558fb49c19e3
Stanislav <stanislav.freelancer2014@gmail.com>
2016-10-17 11:23:56 +0300

    Backend index page

--- backend/views/site/index.php
+++ backend/views/site/index.php
@@ -1,10 +1,43 @@
 <?php
+use \common\models\Company;
+use \common\models\Bonus;
+use \yii\helpers\Url;
+use \common\models\Review;
+use \common\models\Categorie;
+use \common\models\Country;
 
 /* @var $this yii\web\View */
 
 $this->title = 'Dashboard';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div>
-    <h3>Hello world</h3>
+<div class="span10">
+    <div class="row-fluid">
+        <a class="quick-button metro blue span2" href="<?= Url::to(['company/index']) ?>">
+            <i class="icon-building"></i>
+            <p>Companies</p>
+            <span class="badge"><?= Company::find()->count() ?></span>
+        </a>
+        <a class="quick-button metro yellow span2" href="<?= Url::to(['bonus/index']) ?>">
+            <i class="icon-gift"></i>
+            <p>Bonuses</p>
+            <span class="badge"><?= Bonus::find()->count(); ?></span>
+        </a>
+        <a class="quick-button metro pink span2" href="<?= Url::to(['review/index']) ?>">
+            <i class="icon-eye-open"></i>
+            <p>Reviews</p>
+            <span class="badge"><?= Review::find()->where(['type' => Review::REVIEW_TYPE])->count() ?></span>
+        </a>
+        <a class="quick-button metro green span2" href="<?= Url::to(['category/index']) ?>">
+            <i class="icon-eye-open"></i>
+            <p>Categories</p>
+            <span class="badge"><?= Categorie::find()->count() ?></span>
+        </a>
+        <a class="quick-button metro red span2" href="<?= Url::to(['country/index']) ?>">
+            <i class="icon-flag"></i>
+            <p>Countries</p>
+            <span class="badge"><?= Country::find()->count() ?></span>
+        </a>
+        <div class="clearfix"></div>
+    </div>
 </div>
--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,16 +1,18 @@
-
-
 <companies-list params="{companies_list}"></companies-list>
 <?php $this->registerJsFile('/js/companies_list.js', ['depends' => [frontend\assets\AppAsset::className()]]); ?>
 
 <!-- .betting-sites-items -->
 
 <div class="static-content">
-	<p>Welcome to Bonuses Online the site that allows you to find and compare the latest offers from online UK gambling sites.
-		There is something for everyone on this site, no matter what your game of choice is.</p>
+    <p>Welcome to Bonuses Online the site that allows you to find and compare the latest offers from online UK gambling
+        sites.
+        There is something for everyone on this site, no matter what your game of choice is.</p>
 
-		<p>Like playing at the casinos? Play from the comfort of your sofa and receive the latest bonuses from a range of online casinos. Enjoy a flutter? Place your free bets online and choose from top offers from the UK’s top bookmakers. Does bingo call to you? Get free bingo games and more when you select one of the offers from the UK’s biggest online bingo halls.<br/>
-			Prefer poker? Play with free chips in the UK’s most popular online poker games.</p>
-		</div><!-- .static-content -->
+    <p>Like playing at the casinos? Play from the comfort of your sofa and receive the latest bonuses from a range of
+        online casinos. Enjoy a flutter? Place your free bets online and choose from top offers from the UK’s top
+        bookmakers. Does bingo call to you? Get free bingo games and more when you select one of the offers from the
+        UK’s biggest online bingo halls.<br/>
+        Prefer poker? Play with free chips in the UK’s most popular online poker games.</p>
+</div><!-- .static-content -->
 
-	</div>
+</div>

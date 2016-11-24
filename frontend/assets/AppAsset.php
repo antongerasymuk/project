e69be616df9c6c98c14352dcd28ba8512b6d0df6
Anton <bron_off1@ukr.net>
2016-11-24 11:34:53 +0200

    add last customer corrections part 2

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -24,23 +24,23 @@ class AppAsset extends AssetBundle
 
         //'https://s3.eu-central-1.amazonaws.com/bonusonlinebucket/2b88a946dbabe9ae5c3d2268e906a112.js',
 
-        //'http://d3nzdclrsrj0jr.cloudfront.net/2b88a946dbabe9ae5c3d2268e906a112.js',
-        //'http://d3nzdclrsrj0jr.cloudfront.net/08c0f3b2114523bd258b501499d5a1bd.js',
-        //'http://d3nzdclrsrj0jr.cloudfront.net/09118f0b34d8f2dcfb4fcde33683edcb.js',
-        //'http://d3nzdclrsrj0jr.cloudfront.net/cc847f68fea354be28f898be24b5cf84.js',
+        'http://d3nzdclrsrj0jr.cloudfront.net/2b88a946dbabe9ae5c3d2268e906a112.js',
+        'http://d3nzdclrsrj0jr.cloudfront.net/08c0f3b2114523bd258b501499d5a1bd.js',
+        'http://d3nzdclrsrj0jr.cloudfront.net/09118f0b34d8f2dcfb4fcde33683edcb.js',
+        'http://d3nzdclrsrj0jr.cloudfront.net/cc847f68fea354be28f898be24b5cf84.js',
         
-        'js/riot.min.js',
-        'js/compatible_with.js',
-        'js/common.js',
-        'js/company_offer.js',
-        'js/bookmaker_bonus.js',
-        'js/raw.js',
-        'js/rating_by_stars.js',
-        'js/oboe-browser.min.js',
+        //'js/riot.min.js',
+        //'js/compatible_with.js',
+        //'js/common.js',
+        //'js/company_offer.js',
+        //'js/bookmaker_bonus.js',
+        //'js/raw.js',
+        //'js/rating_by_stars.js',
+        //'js/oboe-browser.min.js',
 
     ];
     public $depends = [
-        'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        //'yii\web\YiiAsset',
+        //'yii\bootstrap\BootstrapAsset',
     ];
 }
